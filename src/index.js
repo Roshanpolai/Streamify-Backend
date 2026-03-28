63743e89851674c0f5f8e77e53982f0ded66f11a
Roshan polai <roshanpolai3@gmail.com>
2026-03-28 14:22:17 +0530

    add morgan for logging and update route structure in app.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { app } from "./app.js";
+import app from "./app.js";
 
 // Load environment variables
 dotenv.config({
   path: "./.env",
 });
 
-// Connect to MongoDB and start server
+// Connect MongoDB and start server
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`✅ Server is running at port: ${process.env.PORT || 8000}`);
+      console.log(` Server is running at port: ${process.env.PORT || 8000}`);
     });
   })
   .catch((err) => {
-    console.log("❌ MONGO DB connection failed !!!", err);
-  });
+    console.log(" MONGO DB connection failed !!!", err);
+  }); 
